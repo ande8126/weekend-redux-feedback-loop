5c6f6456fea6cafd1fcc8119186643531d3dce69
Pat Anderson <patrick.b.anderson@gmail.com>
2021-04-02 16:59:14 -0500

    added input and dispatch for Feeling.jsx, reducer is working

--- src/components/Feeling/Feeling.jsx
+++ src/components/Feeling/Feeling.jsx
@@ -1,9 +1,35 @@
-
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 const Feeling = () => {
+    //variable to bring in dispatch
+    const dispatch = useDispatch();
+    //local state to hold score temporarily
+    const [ feelingScore, setFeelingScore ] = useState(0);
+    //function to handle score submission
+    const handleFeeling = (event) =>{;
+        if( event.target.value > 5 ){
+            alert('Please Enter a value between 1-5')
+            setFeelingScore('')
+        }//end limiter
+        else{
+            setFeelingScore(event.target.value)
+        }//end set score
+    }//end handleFeeling
+    
+    //use dispatch to send feelingScore to redux object
+    const sendFeeling = ( score )=>{
+        dispatch({ type: 'sendFeeling', payload: score })
+    }
+
+    
+
+
     return (
-        <div>
-            
-        </div>
+        <>
+            <h2>How are you feeling today?</h2>
+            <input value={feelingScore} type="number" onChange={handleFeeling}/>
+            <button onClick={() => sendFeeling( feelingScore )}>Next</button>
+        </>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -3,8 +3,37 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
 
 
+//reducers go here
+const surveyReducer = ( state = {}, action )=>{
+    if ( action.type === 'sendFeeling' ){
+        state = {...state, feeling: action.payload}
+    }
+    if ( action.type === 'sendUnderstanding' ){
+        state = {...state, understanding: action.payload}
+    }
+    if ( action.type === 'sendSupport' ){
+        state = {...state, support: action.payload}
+    }
+    if ( action.type === 'sendComments' ){
+        state = {...state, comments: action.payload}
+    }
+    return state;
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//store goes here
+const store = createStore(
+    combineReducers({
+        surveyReducer: surveyReducer
+    }),
+    applyMiddleware( logger )
+)
+
+
+//wrap App in provider
+ReactDOM.render(<Provider store = { store }><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
