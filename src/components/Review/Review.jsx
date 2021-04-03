a19130bbcfc2f0c534c610fdf3eb27ebaead6981
Pat Anderson <patrick.b.anderson@gmail.com>
2021-04-02 22:46:49 -0500

    survey results showing up on DOM in Review

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const Review = () => {
     //useSelecter to bring in survey object from Redux
@@ -8,9 +9,21 @@ const Review = () => {
         return store.surveyReducer
     })
 
+    const sendResults = () =>{
+        console.log( 'in sendResults');
+        axios.post( '/')
+    }
+
     return (
         <>
-            <p>{JSON.stringify(survey)}</p>
+            <h2>Review Your Feedback</h2>
+            <p>Feelings: {survey.feeling}</p>
+            <p>Understanding: {survey.understanding}</p>
+            <p>Support: {survey.support}</p>
+            <p>Comments: {survey.comments}</p>
+            <Link to = "/submitted">
+                <button onClick={sendResults}>Submit</button>
+            </Link>
         </>
     )
 }
